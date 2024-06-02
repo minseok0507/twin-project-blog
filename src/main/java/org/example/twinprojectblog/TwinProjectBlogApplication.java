f4078249ffe036b78ff62dd8469f11012aaa4869
minseok0507 <jms892812@gmail.com>
2024-06-02 18:02:32 +0900

    메인페이지 프론트 작업

---
+++ src/main/java/org/example/twinprojectblog/TwinProjectBlogApplication.java
@@ -0,0 +1,13 @@
+package org.example.twinprojectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinProjectBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwinProjectBlogApplication.class, args);
+    }
+
+}
